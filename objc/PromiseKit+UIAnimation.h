ed76081d1838b566913b88d3a1b626f8c1212483
yoshida.masafumi <yoshida.masafumi@dena.jp>
2014-07-11 18:45:30 +0900

    add uianimation promise

---
+++ objc/PromiseKit+UIAnimation.h
@@ -0,0 +1,33 @@
+//
+//  UIView+PromiseKit_UIAnimation.h
+//  YahooDenaStudy
+//
+//  Created by Masafumi Yoshida on 2014/07/11.
+//  Copyright (c) 2014年 DeNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PMKPromise;
+
+@interface UIView (PromiseKit_UIAnimation)
++ (PMKPromise *)promiseAnimationWithDuration:(NSTimeInterval)duration
+                                  animations:(void (^)(void))animations;
+
++ (PMKPromise *)promiseAnimationWithDuration:(NSTimeInterval)duration
+                                       delay:(NSTimeInterval)delay
+                                     options:(UIViewAnimationOptions)options
+                                  animations:(void (^)(void))animations;
+
++ (PMKPromise *)promiseAnimateKeyframesWithDuration:(NSTimeInterval)duration
+                                              delay:(NSTimeInterval)delay
+                                            options:(UIViewKeyframeAnimationOptions)options
+                                         animations:(void (^)(void))animations;
+
++ (PMKPromise *)promiseAnimateWithDuration:(NSTimeInterval)duration
+                                     delay:(NSTimeInterval)delay
+                    usingSpringWithDamping:(CGFloat)dampingRatio
+                     initialSpringVelocity:(CGFloat)velocity
+                                   options:(UIViewAnimationOptions)options
+                                animations:(void (^)(void))animations;
+@end
---
+++ objc/PromiseKit+UIAnimation.m
@@ -0,0 +1,88 @@
+//
+//  UIView+PromiseKit_UIAnimation.m
+//  YahooDenaStudy
+//
+//  Created by Masafumi Yoshida on 2014/07/11.
+//  Copyright (c) 2014年 DeNA. All rights reserved.
+//
+
+#import "PromiseKit+UIAnimation.h"
+#import "PromiseKit+Foundation.h"
+#import "PromiseKit/Promise.h"
+
+@implementation UIView (PromiseKit_UIAnimation)
+
+
++ (PMKPromise *)promiseAnimationWithDuration:(NSTimeInterval)duration
+                                  animations:(void (^)(void))animations{
+    
+    NSAssert([NSThread isMainThread],@"require main thread");
+    
+    return [PMKPromise new:^(PMKPromiseFulfiller fulfiller, PMKPromiseRejecter rejunker){
+        
+        [UIView animateWithDuration:duration
+                         animations:animations
+                         completion:^(BOOL finished) {
+                             fulfiller([NSNumber numberWithBool:finished]);
+                         }];
+        
+    }];
+}
+
++ (PMKPromise *)promiseAnimationWithDuration:(NSTimeInterval)duration
+                                       delay:(NSTimeInterval)delay
+                                     options:(UIViewAnimationOptions)options
+                                  animations:(void (^)(void))animations{
+    NSAssert([NSThread isMainThread],@"require main thread");
+    
+    return [PMKPromise new:^(PMKPromiseFulfiller fulfiller, PMKPromiseRejecter rejunker){
+        
+        [UIView animateWithDuration:duration
+                              delay:delay
+                            options:options
+                         animations:animations
+                         completion:^(BOOL finished) {
+                             fulfiller([NSNumber numberWithBool:finished]);
+                         }];
+        
+    }];
+}
+
++ (PMKPromise *)promiseAnimateKeyframesWithDuration:(NSTimeInterval)duration
+                                              delay:(NSTimeInterval)delay
+                                            options:(UIViewKeyframeAnimationOptions)options
+                                         animations:(void (^)(void))animations {
+    NSAssert([NSThread isMainThread],@"require main thread");
+    
+    return [PMKPromise new:^(PMKPromiseFulfiller fulfiller, PMKPromiseRejecter rejunker){
+        
+        [UIView animateKeyframesWithDuration:duration delay:delay options:options animations:animations completion:^(BOOL finished) {
+            
+        }];
+    }];
+}
+
++ (PMKPromise *)promiseAnimateWithDuration:(NSTimeInterval)duration
+                                     delay:(NSTimeInterval)delay
+                    usingSpringWithDamping:(CGFloat)dampingRatio
+                     initialSpringVelocity:(CGFloat)velocity
+                                   options:(UIViewAnimationOptions)options
+                                animations:(void (^)(void))animations{
+    NSAssert([NSThread isMainThread],@"require main thread");
+    
+    return [PMKPromise new:^(PMKPromiseFulfiller fulfiller, PMKPromiseRejecter rejunker){
+        
+        [UIView animateWithDuration:duration
+                              delay:delay
+             usingSpringWithDamping:dampingRatio
+              initialSpringVelocity:velocity
+                            options:options
+                         animations:animations
+                         completion:^(BOOL finished) {
+                             fulfiller([NSNumber numberWithBool:finished]);
+        }];
+    }];
+}
+
+
+@end
