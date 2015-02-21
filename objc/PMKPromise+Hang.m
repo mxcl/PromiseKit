f0c6c679bfe59148a6fdc250a3351e6baccf1fbc
Max Howell <mxcl@me.com>
2015-02-21 11:38:05 -0600

    `hang` to synchronously obtain a promise’s value
    Closes #116
    
    Tag 1.3.0

---
+++ objc/PMKPromise+Hang.m
@@ -0,0 +1,26 @@
+#import "PromiseKit/Promise+Hang.h"
+
+@implementation PMKPromise (Hang)
+
++ (id)hang:(PMKPromise *)promise {
+    if (promise.pending) {
+        static CFRunLoopSourceContext context;
+
+        CFRunLoopRef runLoop = CFRunLoopGetCurrent();
+        CFRunLoopSourceRef runLoopSource = CFRunLoopSourceCreate(NULL, 0, &context);
+        CFRunLoopAddSource(runLoop, runLoopSource, kCFRunLoopDefaultMode);
+
+        promise.finally(^{
+            CFRunLoopStop(runLoop);
+        });
+        while (promise.pending) {
+            CFRunLoopRun();
+        }
+        CFRunLoopRemoveSource(runLoop, runLoopSource, kCFRunLoopDefaultMode);
+        CFRelease(runLoopSource);
+    }
+
+    return [promise value];
+}
+
+@end
---
+++ objc/PromiseKit/Promise+Hang.h
@@ -0,0 +1,13 @@
+#import <PromiseKit/fwd.h>
+
+@interface PMKPromise (Hang)
+
+/**
+ 0.2.4.6.8.0.2.4.6.8.0.2.4.6.8.0.2.4.6.8.0.2.4.6.8.0.2.4.6.8.0.2.4.6.8.0.2
+ Literally hangs this thread until the promise has resolved. By all means
+ use this for testing and debugging, but please! Don’t leave it in
+ production code! It could literally *hang* your app! The implementation
+ uses {CF|NS}RunLoop.
+*/
++ (id)hang:(PMKPromise *)promise;
+@end
