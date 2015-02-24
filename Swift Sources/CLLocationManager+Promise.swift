ed6a81fae3dfdd68fddf97355835ea46068af24f
Max Howell <mxcl@me.com>
2015-02-24 15:58:20 -0600

    Automatic and specific request type detection
    Closes #151

--- Swift Sources/CLLocationManager+Promise.swift
+++ Swift Sources/CLLocationManager+Promise.swift
@@ -18,25 +18,20 @@ private class LocationManager: CLLocationManager, CLLocationManagerDelegate {
         super.init()
         PMKRetain(self)
         delegate = self
-    #if os(iOS)
-        requestWhenInUseAuthorization()
-    #endif
     }
 }
 
 private class AuthorizationCatcher: CLLocationManager, CLLocationManagerDelegate {
     let fulfill: (CLAuthorizationStatus) -> Void
 
-    init(_ fulfill: (CLAuthorizationStatus)->()) {
-        self.fulfill = fulfill
+    init(fulfiller: (CLAuthorizationStatus)->(), auther: (CLLocationManager)->()) {
+        fulfill = fulfiller
         super.init()
         let status = CLLocationManager.authorizationStatus()
         if status == .NotDetermined {
-            self.delegate = self
+            delegate = self
             PMKRetain(self)
-        #if os(iOS)
-            requestAlwaysAuthorization()
-        #endif
+            auther(self)
         } else {
             fulfill(status)
         }
@@ -50,19 +45,72 @@ private class AuthorizationCatcher: CLLocationManager, CLLocationManagerDelegate
     }
 }
 
+
+private func auther(requestAuthorizationType: CLLocationManager.RequestAuthorizationType)(manager: CLLocationManager)
+{
+    func hasInfoPListKey(key: String) -> Bool {
+        let value = NSBundle.mainBundle().objectForInfoDictionaryKey("NSLocationAlwaysUsageDescription") as? String ?? ""
+        return !value.isEmpty
+    }
+
+  #if os(iOS)
+    switch requestAuthorizationType {
+    case .Automatic:
+        let always = hasInfoPListKey("NSLocationAlwaysUsageDescription")
+        let whenInUse = hasInfoPListKey("NSLocationWhenInUseUsageDescription")
+        if hasInfoPListKey("NSLocationAlwaysUsageDescription") {
+            manager.requestAlwaysAuthorization()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+    case .WhenInUse:
+
+        manager.requestWhenInUseAuthorization()
+        break
+    case .Always:
+        manager.requestAlwaysAuthorization()
+        break
+
+    }
+  #endif
+}
+
+
 extension CLLocationManager {
+
+    public enum RequestAuthorizationType {
+        case Automatic
+        case Always
+        case WhenInUse
+    }
+  
     /**
       Returns the most recent CLLocation.
+
+      @param requestAuthorizationType We read your Info plist and try to
+      determine the authorization type we should request automatically. If you
+      want to force one or the other, change this parameter from its default
+      value.
      */
-    public class func promise() -> Promise<CLLocation> {
-        return promise().then { (locations: [CLLocation]) -> CLLocation in
-            return last(locations)!
+    public class func promise(requestAuthorizationType: RequestAuthorizationType = .Automatic) -> Promise<CLLocation> {
+        let p: Promise<[CLLocation]> = promise(requestAuthorizationType: requestAuthorizationType)
+        return p.then { (locations)->CLLocation in
+            return locations[locations.count - 1]
         }
     }
 
-    public class func promise() -> Promise<[CLLocation]> {
+    /**
+      Returns the first batch of location objects a CLLocationManager instance
+      provides.
+     */
+    public class func promise(requestAuthorizationType: RequestAuthorizationType = .Automatic) -> Promise<[CLLocation]> {
+        return promise(yield: auther(requestAuthorizationType))
+    }
+
+    private class func promise(yield: (CLLocationManager)->() = { _ in }) -> Promise<[CLLocation]> {
         let deferred = Promise<[CLLocation]>.defer()
         let manager = LocationManager(deferred.fulfill, deferred.reject)
+        yield(manager)
         manager.startUpdatingLocation()
         deferred.promise.finally {
             manager.delegate = nil
@@ -78,9 +126,14 @@ extension CLLocationManager {
       and errors only. Thus your catch handler is always catching failures
       and not being abused for logic.
      */
-    public class func requestAlwaysAuthorization() -> Promise<CLAuthorizationStatus> {
+    public class func requestAuthorization(type: RequestAuthorizationType = .Automatic) -> Promise<CLAuthorizationStatus> {
         let d = Promise<CLAuthorizationStatus>.defer()
-        AuthorizationCatcher(d.fulfill)
+        AuthorizationCatcher(fulfiller: d.fulfill, auther: auther(type))
         return d.promise
+    }
+
+    @availability(*, deprecated=1.3.0)
+    public class func requestAlwaysAuthorization() -> Promise<CLAuthorizationStatus> {
+        return requestAuthorization(type: .Always)
     }
 }
