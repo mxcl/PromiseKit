161bd47d56ba03d88925376f24be29521add04c3
Max Howell <mxcl@me.com>
2015-03-17 12:42:56 -0500

    Add OS X target

--- Swift Sources/PromiseKit.h
+++ Swift Sources/PromiseKit.h
@@ -3,8 +3,10 @@
 FOUNDATION_EXPORT double PromiseKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
 
+#if !TARGET_OS_MAC
 // FIXME
 @import AssetsLibrary;
+#endif
 
 // for convenience and better error messages if you don’t
 // have OMGHTTPURLRQ available and to hand
