d8745d2903a5b18e9fc1c7ec441bad3d426e95be
Max Howell <mxcl@me.com>
2015-06-22 14:32:24 -0500

    Another after(life:) test

--- Tests/Categories/TestNSObject.swift
+++ Tests/Categories/TestNSObject.swift
@@ -45,4 +45,31 @@ class TestNSObject: XCTestCase {
 
         waitForExpectationsWithTimeout(1, handler: nil)
     }
+
+    func testMultiObserveAfterlife() {
+        let ex1 = expectationWithDescription("")
+        let ex2 = expectationWithDescription("")
+        var killme: NSObject!
+
+        autoreleasepool {
+
+            func innerScope() {
+                killme = NSObject()
+                after(life: killme).then { _ -> Void in
+                    ex1.fulfill()
+                }
+                after(life: killme).then { _ -> Void in
+                    ex2.fulfill()
+                }
+            }
+
+            innerScope()
+
+            after(0.2).then {
+                killme = nil
+            }
+        }
+
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
 }
