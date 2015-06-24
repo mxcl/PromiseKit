878408413dc936b5f3acc05dc1f55181474b734a
Max Howell <mxcl@me.com>
2015-06-24 13:12:29 -0500

    Comply with Swift 2 function labeling rules

--- Tests/Categories/TestCoreLocation.swift
+++ Tests/Categories/TestCoreLocation.swift
@@ -111,7 +111,7 @@ class TestCLGeocoder: XCTestCase {
 
 /////////////////////////////////////////////////////////////// resources
 
-func swizzle(foo: AnyClass, from: Selector, isClassMethod: Bool = false, @noescape body: () -> Void) {
+func swizzle(foo: AnyClass, _ from: Selector, isClassMethod: Bool = false, @noescape body: () -> Void) {
     let get: (AnyClass!, Selector) -> Method = isClassMethod ? class_getClassMethod : class_getInstanceMethod
     let originalMethod = get(foo, from)
     let swizzledMethod = get(foo, Selector("pmk_\(from)"))
