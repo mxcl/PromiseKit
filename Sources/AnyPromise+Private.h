175fd8a3c608c316dbd73dc0164cc213d679eb03
Max Howell <mxcl@me.com>
2015-07-31 20:46:55 -0500

    nullable/nonnull
    Fixes #206

--- Sources/AnyPromise+Private.h
+++ Sources/AnyPromise+Private.h
@@ -8,7 +8,7 @@
         aa; \
     })
 #else
-    static inline NSPointerArray *NSPointerArrayMake(NSUInteger count) {
+    static inline NSPointerArray * __nonnull NSPointerArrayMake(NSUInteger count) {
       #pragma clang diagnostic push
       #pragma clang diagnostic ignored "-Wdeprecated-declarations"
         NSPointerArray *aa = [[NSPointerArray class] respondsToSelector:@selector(strongObjectsPointerArray)]
@@ -26,11 +26,11 @@
 #import "AnyPromise.h"
 
 @interface AnyPromise (Swift)
-- (void)pipe:(void (^)(id))body;
-- (AnyPromise *)initWithBridge:(void (^)(PMKResolver))resolver;
+- (void)pipe:(void (^ __nonnull)(id __nonnull))body;
+- (AnyPromise * __nonnull)initWithBridge:(void (^ __nonnull)(PMKResolver __nonnull))resolver;
 @end
 
-extern NSError *PMKProcessUnhandledException(id thrown);
+extern NSError * __nullable PMKProcessUnhandledException(id __nonnull thrown);
 
 // TODO really this is not valid, we should instead nest the errors with NSUnderlyingError
 // since a special error subclass may be being used and we may not setup it up correctly
