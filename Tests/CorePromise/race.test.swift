2a25f22384220b30c434a8e13f66323a46ca18e3
Rachel Bobbins <rbobbins@stitchfix.com>
2016-03-10 12:17:54 -0800

    Updated race to support array of promises, not just varargs

--- Tests/CorePromise/race.test.swift
+++ Tests/CorePromise/race.test.swift
@@ -2,7 +2,7 @@ import XCTest
 import PromiseKit
 
 class RaceTestCase_Swift: XCTestCase {
-    func test1() {
+    func test1WithVariadicArgs() {
         let ex = expectationWithDescription("")
         race(after(0.01).then{ 1 }, after(1.0).then{ 2 }).then { index -> Void in
             XCTAssertEqual(index, 1)
@@ -11,9 +11,27 @@ class RaceTestCase_Swift: XCTestCase {
         waitForExpectationsWithTimeout(1, handler: nil)
     }
     
-    func test2() {
+    func test2WithVariadicArgs() {
         let ex = expectationWithDescription("")
         race(after(1.0).then{ 1 }, after(0.01).then{ 2 }).then { index -> Void in
+            XCTAssertEqual(index, 2)
+            ex.fulfill()
+        }
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
+
+    func test1WithArrayArgs() {
+        let ex = expectationWithDescription("")
+        race([after(0.01).then{ 1 }, after(1.0).then{ 2 }]).then { index -> Void in
+            XCTAssertEqual(index, 1)
+            ex.fulfill()
+        }
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
+
+    func test2WithArrayArgs() {
+        let ex = expectationWithDescription("")
+        race([after(1.0).then{ 1 }, after(0.01).then{ 2 }]).then { index -> Void in
             XCTAssertEqual(index, 2)
             ex.fulfill()
         }
