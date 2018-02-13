a8616e56694fd8a921978313873e8e24278670c9
Max Howell <mxcl@me.com>
2018-02-13 16:20:59 -0500

    Provide `wrap` but deprecated

--- Sources/Deprecations.swift
+++ Sources/Deprecations.swift
@@ -1,8 +1,34 @@
-//
-//  Deprecations.swift
-//  PromiseKit
-//
-//  Created by Max Howell on 2/13/18.
-//
+@available(*, deprecated: 5.0)
+public func wrap<T>(_ body: (@escaping (T?, Error?) -> Void) throws -> Void) -> Promise<T> {
+    return Promise { seal in
+        try body(seal.resolve)
+    }
+}
 
-import Foundation
+@available(*, deprecated: 5.0)
+public func wrap<T>(_ body: (@escaping (T, Error?) -> Void) throws -> Void) -> Promise<T>  {
+    return Promise { seal in
+        try body(seal.resolve)
+    }
+}
+
+@available(*, deprecated: 5.0)
+public func wrap<T>(_ body: (@escaping (Error?, T?) -> Void) throws -> Void) -> Promise<T> {
+    return Promise { seal in
+        try body(seal.resolve)
+    }
+}
+
+@available(*, deprecated: 5.0)
+public func wrap(_ body: (@escaping (Error?) -> Void) throws -> Void) -> Promise<Void> {
+    return Promise { seal in
+        try body(seal.resolve)
+    }
+}
+
+@available(*, deprecated: 5.0)
+public func wrap<T>(_ body: (@escaping (T) -> Void) throws -> Void) -> Promise<T> {
+    return Promise { seal in
+        try body(seal.fulfill)
+    }
+}
