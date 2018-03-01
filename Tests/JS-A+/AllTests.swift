d012597ec0fe1e900c41f0591d02e086e684711d
Lois Di Qual <lois.diqual@gmail.com>
2018-03-01 01:47:39 -0800

    Proper formatting

--- Tests/JS-A+/AllTests.swift
+++ Tests/JS-A+/AllTests.swift
@@ -23,13 +23,30 @@ class MockNodeEnvironment {
                     return
                 }
                 
-                let otherArguments = arguments.dropFirst().flatMap { ($0 as? JSValue)?.toObject() as? CVarArg }
+                let otherArguments = arguments.dropFirst()
                 if otherArguments.count == 0 {
                     print(format)
                 } else {
+                    
+                    let otherArguments = otherArguments.flatMap { $0 as? JSValue }
                     let format = format.toString().replacingOccurrences(of: "%s", with: "%@")
-                    let output = String(format: format, arguments: otherArguments)
-//                    print(format)
+                    
+                    // TODO: fix this format hack
+                    let expectedTypes = " \(format)".split(separator: "%").dropFirst().flatMap { $0.first }.map { String($0) }
+                    
+                    let typedArguments = otherArguments.enumerated().flatMap { index, value -> CVarArg? in
+                        let expectedType = expectedTypes[index]
+                        let converted: CVarArg
+                        switch expectedType {
+                        case "s": converted = value.toString()
+                        case "d": converted = value.toInt32()
+                        case "f": converted = value.toDouble()
+                        default: converted = value.toString()
+                        }
+                        return converted
+                    }
+                    
+                    let output = String(format: format, arguments: typedArguments)
                     print(output)
                 }
             }
