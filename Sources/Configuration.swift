a7fea1ba9f27a14ccff07c32dea1e2c69af75e5b
Max Howell <mxcl@me.com>
2018-11-24 17:57:27 -0500

    Update Configuration.swift

--- Sources/Configuration.swift
+++ Sources/Configuration.swift
@@ -1,6 +1,12 @@
 import Dispatch
 
-/// PromiseKit’s configurable parameters
+/**
+ PromiseKit’s configurable parameters.
+
+ Do not change these after any Promise machinery executes as the configuration object is not thread-safe.
+
+ We would like it to be, but sadly `Swift` does not expose `dispatch_once` et al. which is what we used to use in order to make the configuration immutable once first used.
+*/
 public struct PMKConfiguration {
     /// The default queues that promises handlers dispatch to
     public var Q: (map: DispatchQueue?, return: DispatchQueue?) = (map: DispatchQueue.main, return: DispatchQueue.main)
