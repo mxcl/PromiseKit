68763cf30161fb0a5e254de867488f13ca7cc5ef
Garth Snyder <garth@garthsnyder.com>
2018-12-17 23:18:50 -0800

    Revert commits 0e902 and d1a38; these were rebased in from master

--- Sources/Resolver.swift
+++ Sources/Resolver.swift
@@ -8,7 +8,7 @@ public final class Resolver<T> {
 
     deinit {
         if case .pending = box.inspect() {
-            conf.logHandler(.pendingPromiseDeallocated)
+            conf.logHandler(.waitOnMainThread)
         }
     }
 }
