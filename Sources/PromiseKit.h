22fc4809da9c0749b2a8ef9119e4637d233d7650
Dan Federman <dan.federman@airbnb.com>
2019-01-25 11:29:36 -0800

    Make umbrella header import all public .h files with proper public import syntax

--- Sources/PromiseKit.h
+++ Sources/PromiseKit.h
@@ -1,7 +1,12 @@
-#import "fwd.h"
-#import "AnyPromise.h"
-
 #import <Foundation/NSObjCRuntime.h>  // `FOUNDATION_EXPORT`
 
 FOUNDATION_EXPORT double PromiseKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromiseKit/PublicHeader.h>
+
+#import <PromiseKit/AnyPromise.h>
+#import <PromiseKit/CLGeocoder+AnyPromise.h>
+#import <PromiseKit/CLLocationManager+AnyPromise.h>
+#import <PromiseKit/fwd.h>
+#import <PromiseKit/PMKCoreLocation.h>
