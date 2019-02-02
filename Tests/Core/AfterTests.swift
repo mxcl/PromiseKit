aa24737dfb07aab2213718397b4bc8d3f2801f20
Max Howell <mxcl@me.com>
2019-02-02 12:47:24 -0500

    v7 initial changes
    * Swift 5 only
    * No ObjC
    * No deprecations
    * No submodules
    * Uses SwiftPM to generate an `.xcodeproj`

---
+++ Tests/Core/AfterTests.swift
@@ -0,0 +1,34 @@
+import PromiseKit
+import XCTest
+
+class AfterTests: XCTestCase {
+    func testZero() {
+        let ex2 = expectation(description: "")
+        after(seconds: 0).done(ex2.fulfill)
+        waitForExpectations(timeout: 2, handler: nil)
+
+        let ex3 = expectation(description: "")
+        after(.seconds(0)).done(ex3.fulfill)
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+
+    func testNegative() {
+        let ex2 = expectation(description: "")
+        after(seconds: -1).done(ex2.fulfill)
+        waitForExpectations(timeout: 2, handler: nil)
+
+        let ex3 = expectation(description: "")
+        after(.seconds(-1)).done(ex3.fulfill)
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+
+    func testPositive() {
+        let ex2 = expectation(description: "")
+        after(seconds: 1).done(ex2.fulfill)
+        waitForExpectations(timeout: 2, handler: nil)
+
+        let ex3 = expectation(description: "")
+        after(.seconds(1)).done(ex3.fulfill)
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+}
