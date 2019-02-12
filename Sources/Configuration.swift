277944ff1943b12626a64e35da115e66a05adeb9
Garth Snyder <garth@garthsnyder.com>
2019-02-12 00:51:16 +0000

    Generalize handler dispatch mechanisms

--- Sources/Configuration.swift
+++ Sources/Configuration.swift
@@ -8,8 +8,14 @@ import Dispatch
  We would like it to be, but sadly `Swift` does not expose `dispatch_once` et al. which is what we used to use in order to make the configuration immutable once first used.
 */
 public struct PMKConfiguration {
-    /// The default queues that promises handlers dispatch to
-    public var Q: (map: DispatchQueue?, return: DispatchQueue?) = (map: DispatchQueue.main, return: DispatchQueue.main)
+    /// Backward compatibility: default DispatchQueues that promise handlers dispatch to
+    public var Q: (map: DispatchQueue?, return: DispatchQueue?) {
+        get { return (map: D.map as? DispatchQueue, return: D.return as? DispatchQueue) }
+        set { D = (map: newValue.map ?? CurrentThreadDispatcher(), return: newValue.return ?? CurrentThreadDispatcher()) }
+    }
+
+    /// The default Dispatchers that promise handlers dispatch to
+    public var D: (map: Dispatcher, return: Dispatcher) = (map: DispatchQueue.main, return: DispatchQueue.main)
 
     /// The default catch-policy for all `catch` and `resolve`
     public var catchPolicy = CatchPolicy.allErrorsExceptCancellation
