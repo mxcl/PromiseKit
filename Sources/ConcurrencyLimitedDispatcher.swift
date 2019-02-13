7019858d2746995592ea0ff8cd8f12495ae72a58
Garth Snyder <garth@garthsnyder.com>
2019-02-12 16:09:11 -0800

    Grooming, doc comments, NSLock -> DispatchQueue

--- Sources/ConcurrencyLimitedDispatcher.swift
+++ Sources/ConcurrencyLimitedDispatcher.swift
@@ -6,25 +6,25 @@ import Foundation
 class ConcurrencyLimitedDispatcher: Dispatcher {
     
     let queue: Dispatcher
-    let serialEntryQueue: DispatchQueue
+    let serializer: DispatchQueue = DispatchQueue(label: "CLD serializer")
     
     private let semaphore: DispatchSemaphore
     
     /// A `PromiseKit` `Dispatcher` that allows no more than X simultaneous
     /// executions at once.
     ///
-    /// - Parameter limit: The number of executions that may run at once.
-    /// - Parameter queue: The DispatchQueue or Dispatcher on which to perform executions.
-    ///     Should be some form of concurrent queue.
+    /// - Parameters:
+    ///   - limit: The number of executions that may run at once.
+    ///   - queue: The DispatchQueue or Dispatcher on which to perform executions.
+    ///       Should be some form of concurrent queue.
 
     public init(limit: Int, queue: Dispatcher = DispatchQueue.global(qos: .background)) {
         self.queue = queue
-        serialEntryQueue = DispatchQueue(label: "CLD entryway")
         semaphore = DispatchSemaphore(value: limit)
     }
 
     public func dispatch(_ body: @escaping () -> Void) {
-        serialEntryQueue.async {
+        serializer.async {
             self.semaphore.wait()
             self.queue.dispatch {
                 body()
