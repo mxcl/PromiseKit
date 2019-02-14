b68b53a79a5bfc393fcebedc619d79e53cc77b2d
Garth Snyder <garth@garthsnyder.com>
2019-02-14 14:23:02 -0800

    Refactor rate limited dispatchers onto common base

--- Sources/RateLimitedDispatcher.swift
+++ Sources/RateLimitedDispatcher.swift
@@ -11,8 +11,8 @@ import Foundation
 /// For a completely accurate rate limiter that dispatches as rapidly as
 /// possible, see `StrictRateLimitedDispatcher`. But note that this variant
 /// incurs a 
-
-a sliding window, so executions occur as rapidly as
+///
+/// a sliding window, so executions occur as rapidly as
 /// possible without exceeding X in any Y-second period.
 ///
 /// This version implements perfectly accurate timing, so it must keep
@@ -31,109 +31,59 @@ a sliding window, so executions occur as rapidly as
 ///
 /// 100% thread safe.
 
-public class StrictRateLimitedDispatcher: Dispatcher {
+public class RateLimitedDispatcher: RateLimitedDispatcherBase {
     
-    let maxDispatches: Int
-    let interval: TimeInterval
-    let queue: Dispatcher
+    private var tokensInBucket: Double = 0
+    private var lastAccrual: DispatchTime = DispatchTime.now()
+    private var retryWorkItem: DispatchWorkItem? { willSet { retryWorkItem?.cancel() }}
     
-    private let serializer = DispatchQueue(label: "SRLD serializer")
-    
-    private var nScheduled = 0
-    private var unscheduled = Queue<() -> Void>()
-    internal var startTimeHistory: Queue<DispatchTime>
-    private var latestDeadline = DispatchTime(uptimeNanoseconds: 0)
-    
-    private var cleanupNonce: Int64 = 0
-    private var cleanupWorkItem: DispatchWorkItem? { willSet { cleanupWorkItem?.cancel() }}
-    
-    /// A `PromiseKit` Dispatcher that executes no more than X executions every Y
-    /// seconds. This is a sliding window, so executions occur as rapidly as
-    /// possible without exceeding X in any Y-second period.
-    ///
-    /// For a "pretty good" approach to rate limiting that does not consume
-    /// additional storage, see `RateLimitedDispatcher`.
-    ///
-    /// - Parameter maxDispatches: The number of executions that may be dispatched within a given interval.
-    /// - Parameter perInterval: The length of the reference interval, in seconds.
-    /// - Parameter queue: The DispatchQueue or Dispatcher on which to perform executions. May be serial or concurrent.
-    
-    public init(maxDispatches: Int, perInterval interval: TimeInterval, queue: Dispatcher = DispatchQueue.global()) {
-        self.maxDispatches = maxDispatches
-        self.interval = interval
-        self.queue = queue
-        startTimeHistory = Queue<DispatchTime>(maxDepth: maxDispatches)
+    override init(maxDispatches: Int, perInterval interval: TimeInterval, queue: Dispatcher = DispatchQueue.global()) {
+        lastAccrual = DispatchTime.now()
+        super.init(maxDispatches: maxDispatches, perInterval: interval, queue: queue)
+        tokensInBucket = Double(maxDispatches)
     }
     
-    public func dispatch(_ body: @escaping () -> Void) {
-        serializer.async {
-            self.unscheduled.enqueue(body)
-            self.scheduleNext()
-        }
-    }
+    override func dispatchFromQueue() {
     
-    private func scheduleNext() {
+        let now = DispatchTime.now()
+        let tokensPerSecond = Double(maxDispatches) / interval
+        let tokensToAdd = (now - lastAccrual) * tokensPerSecond
+        tokensInBucket = min(Double(maxDispatches), tokensInBucket + tokensToAdd)
+        lastAccrual = now
         
-        cleanupNonce += 1
-        
-        guard nScheduled < maxDispatches else { return }
-        guard !unscheduled.isEmpty else { return }
-        
-        var deadline = DispatchTime.now()
-        if !startTimeHistory.isEmpty {
-            // Use the start time of a previous closure as a time reference. In practice,
-            // past start times will normally be reported and recorded in monotonically
-            // increasing sequence, which yields optimal scheduling. However, this is all
-            // potentially multithreaded, so there are no order guarantees. However, if any
-            // start times ARE out of order, the algorithm is still correct: for each
-            // dispatched item, another may be scheduled interval seconds later. Like kanban.
-            deadline = max(deadline, startTimeHistory.dequeue() + interval)
-        }
-        // Enforce a monotonically increasing outbound schedule to keep calls in order
-        if deadline <= latestDeadline {
-            deadline = DispatchTime(uptimeNanoseconds: latestDeadline.uptimeNanoseconds + 1)
-        }
-        
-        let body = unscheduled.dequeue()
-        // A Dispatcher has no asyncAfter; use the serializer queue for timing
-        serializer.asyncAfter(deadline: deadline) {
-            self.queue.dispatch {
-                let now = DispatchTime.now()
+        var didDispatch = false
+        while tokensInBucket >= 1.0 && !undispatched.isEmpty && nScheduled < maxDispatches {
+            didDispatch = true
+            tokensInBucket -= 1.0
+            nScheduled += 1
+            let body = unscheduled.dequeue()
+            queue.dispatch {
                 self.serializer.async {
-                    self.recordActualStartTime(now)
+                    self.recordActualStart()
                 }
                 body()
             }
         }
-        
-        latestDeadline = deadline
-        nScheduled += 1
-        
-    }
-    
-    private func recordActualStartTime(_ time: DispatchTime) {
-        nScheduled -= 1
-        startTimeHistory.enqueue(time)
-        scheduleNext()
-        if nScheduled == 0 && unscheduled.isEmpty {
-            scheduleCleanup()
+
+        if didDispatch {
+            cleanupNonce += 1
+        } else {
+            scheduleRetry()
         }
+
     }
     
-    private func scheduleCleanup() {
-        cleanupWorkItem = DispatchWorkItem { [ weak self, nonce = self.cleanupNonce ] in
-            self?.cleanup(nonce)
+    private func scheduleRetry() {
+        guard nScheduled == 0 && retryWorkItem == nil else { return }
+        let tokensPerSecond = Double(maxDispatches) / interval
+        let tokenDeficit = 1.0 - tokensInBucket
+        let secondsToGo = tokenDeficit / tokensPerSecond
+        let deadline = lastAccrual + secondsToGo + 1.0E-6
+        let retryWorkItem = DispatchWorkItem {
+            self?.retryWorkItem = nil
+            self?.dispatchFromQueue()
         }
-        serializer.asyncAfter(deadline: DispatchTime.now() + interval, execute: cleanupWorkItem!)
+        serializer.asyncAfter(deadline: deadline, execute: retryWorkItem!)
     }
-    
-    private func cleanup(_ nonce: Int64) {
-        // Calls to cleanup() have to go through the serializer queue, so by by the time
-        // we get here, more activity may have occurred. Ergo, verify nonce.
-        guard nonce == cleanupNonce else { return }
-        startTimeHistory.purge() // We're at least an interval past last start
-        unscheduled.compactStorage()
-        cleanupWorkItem = nil
-    }
-    
+
 }
