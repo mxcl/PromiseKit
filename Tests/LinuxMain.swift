6cfdb232356350b2f720f9061a2492b813261362
Max Howell <mxcl@me.com>
2019-04-07 13:01:20 -0400

    Use Swift 5 to generate linuxmain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,10 @@
 import XCTest
 
-import CorePromise
 import A_
+import CorePromise
 
 var tests = [XCTestCaseEntry]()
-tests += CorePromise.__allTests()
 tests += A_.__allTests()
+tests += CorePromise.__allTests()
 
 XCTMain(tests)
