1e2e1177a3d2d3ff4502ca5cbb760aeadd4514dc
Joachim Deelen <jdeelen@micabo.de>
2021-03-17 16:12:34 +0100

    Split up the PromiseKit package into two targets. One for pure Swift and another for Objective-C Targets must be Swift or Objective-C only. Mixing languages in one target is not supported by SPM. Both targets get integrated into the PromiseKit library product

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -3,31 +3,53 @@
 import PackageDescription
 
 let pkg = Package(name: "PromiseKit")
-pkg.platforms = [
-   .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
-]
 pkg.products = [
-    .library(name: "PromiseKit", targets: ["PromiseKit"]),
+	.library(name: "PromiseKit", targets: ["PromiseKit", "PromiseKitObjC"])
 ]
 
 let pmk: Target = .target(name: "PromiseKit")
 pmk.path = "Sources"
 pmk.exclude = [
-    "AnyPromise.swift",
-    "AnyPromise.m",
-    "PMKCallVariadicBlock.m",
-    "dispatch_promise.m",
-    "join.m",
-    "when.m",
-    "NSMethodSignatureForBlock.m",
-    "after.m",
-    "hang.m",
-    "race.m",
-    "Deprecations.swift"
+	"AnyPromise.m",
+	"PMKCallVariadicBlock.m",
+	"dispatch_promise.m",
+	"join.m",
+	"when.m",
+	"NSMethodSignatureForBlock.m",
+	"after.m",
+	"hang.m",
+	"race.m",
+	"Deprecations.swift"
+]
+
+let pmkObjc: Target = .target(name: "PromiseKitObjC")
+pmkObjc.dependencies = [.target(name: "PromiseKit")]
+pmkObjc.path = "Sources"
+pmkObjc.exclude = [
+	"PMKCallVariadicBlock.m",
+	"after.swift",
+	"AnyPromise.swift",
+	"Box.swift",
+	"Catchable.swift",
+	"Configuration.swift",
+	"CustomStringConvertible.swift",
+	"Deprecations.swift",
+	"Error.swift",
+	"firstly.swift",
+	"Guarantee.swift",
+	"hang.swift",
+	"Info.plist",
+	"LogEvent.swift",
+	"Promise.swift",
+	"race.swift",
+	"Resolver.swift",
+	"Thenable.swift",
+	"when.swift"
 ]
 pkg.swiftLanguageVersions = [.v4, .v4_2, .v5]
 pkg.targets = [
     pmk,
+	pmkObjc,
     .testTarget(name: "APlus", dependencies: ["PromiseKit"], path: "Tests/A+"),
     .testTarget(name: "CorePromise", dependencies: ["PromiseKit"], path: "Tests/CorePromise"),
 ]
