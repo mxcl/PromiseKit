c065234fdc52e21318bed56d5754163b8684cdf3
Max Howell <mxcl@me.com>
2021-06-02 15:53:47 -0400

    Run JS/A+ individually to try and avoid timeouts

--- Tests/A+/JavaScript/AllTests.swift
+++ Tests/A+/JavaScript/AllTests.swift
@@ -10,15 +10,28 @@ import JavaScriptCore
 import PromiseKit
 import XCTest
 
-class AllTests: XCTestCase {
-    func testAll() {
-        let scriptPath = URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("build/build.js")
+class APlusJavaScriptTests: XCTestCase {
+    func test_2_1_2() { runner(test: "2.1.2") }
+    func test_2_1_3() { runner(test: "2.1.3") }
+    func test_2_2_1() { runner(test: "2.2.1") }
+    func test_2_2_2() { runner(test: "2.2.2") }
+    func test_2_2_3() { runner(test: "2.2.3") }
+    func test_2_2_4() { runner(test: "2.2.4") }
+    func test_2_2_5() { runner(test: "2.2.5") }
+    func test_2_2_6() { runner(test: "2.2.6") }
+    func test_2_2_7() { runner(test: "2.2.7") }
+    func test_2_3_1() { runner(test: "2.3.1") }
+    func test_2_3_2() { runner(test: "2.3.2") }
+    func test_2_3_4() { runner(test: "2.3.4") }
+
+    func runner(test testName: String, file: StaticString = #file, line: UInt = #line) {
+        let scriptPath = URL(fileURLWithPath: #filePath).deletingLastPathComponent().appendingPathComponent("build/build.js")
         guard FileManager.default.fileExists(atPath: scriptPath.path) else {
             return print("Skipping A+.js: see README for instructions on how to build")
         }
         
         guard let script = try? String(contentsOf: scriptPath) else {
-            return XCTFail("Couldn't read content of test suite JS file")
+            return XCTFail("Couldn't read content of test suite JS file", file: file, line: line)
         }
         
         let context = JSUtils.sharedContext
@@ -26,7 +39,7 @@ class AllTests: XCTestCase {
         // Add a global exception handler
         context.exceptionHandler = { context, exception in
             guard let exception = exception else {
-                return XCTFail("Unknown JS exception")
+                return XCTFail("Unknown JS exception", file: file, line: line)
             }
             JSUtils.printStackTrace(exception: exception, includeExceptionDescription: true)
         }
@@ -40,7 +53,7 @@ class AllTests: XCTestCase {
         
         // Create adapter
         guard let adapter = JSValue(object: NSDictionary(), in: context) else {
-            fatalError("Couldn't create adapter")
+            return XCTFail("Couldn't create adapter", file: file, line: line)
         }
         adapter.setObject(JSAdapter.resolved, forKeyedSubscript: "resolved" as NSString)
         adapter.setObject(JSAdapter.rejected, forKeyedSubscript: "rejected" as NSString)
@@ -49,15 +62,15 @@ class AllTests: XCTestCase {
         // Evaluate contents of `build.js`, which exposes `runTests` in the global context
         context.evaluateScript(script)
         guard let runTests = context.objectForKeyedSubscript("runTests") else {
-            return XCTFail("Couldn't find `runTests` in JS context")
+            return XCTFail("Couldn't find `runTests` in JS context", file: file, line: line)
         }
         
         // Create a callback that's called whenever there's a failure
         let onFail: @convention(block) (JSValue, JSValue) -> Void = { test, error in
             guard let test = test.toString(), let error = error.toString() else {
-                return XCTFail("Unknown test failure")
+                return XCTFail("Unknown test failure", file: file, line: line)
             }
-            XCTFail("\(test) failed: \(error)")
+            return XCTFail("\(test) failed: \(error)", file: file, line: line)
         }
         let onFailValue: JSValue = JSValue(object: onFail, in: context)
         
@@ -68,13 +81,16 @@ class AllTests: XCTestCase {
         }
         let onDoneValue: JSValue = JSValue(object: onDone, in: context)
         
-        // If there's a need to only run one specific test, uncomment the next line and comment the one after
-        // let testName: JSValue = JSValue(object: "2.3.1", in: context)
-        let testName = JSUtils.undefined
+        guard let testName = JSValue(object: testName, in: context) else {
+            return XCTFail(file: file, line: line)
+        }
+
+        // use this to run *all* tests
+        //let testName = JSUtils.undefined
         
         // Call `runTests`
         runTests.call(withArguments: [adapter, onFailValue, onDoneValue, testName])
-        self.wait(for: [expectation], timeout: 600)
+        wait(for: [expectation], timeout: 30)
     }
 }
 
--- Tests/A+/JavaScript/JSPromise.swift
+++ Tests/A+/JavaScript/JSPromise.swift
@@ -6,7 +6,6 @@
 //
 
 #if !os(Linux) && !os(watchOS)
-// can disable better when we don’t need --generate-linuxmain
 import JavaScriptCore
 import PromiseKit
 import XCTest
@@ -16,34 +15,34 @@ import XCTest
 }
 
 class JSPromise: NSObject, JSPromiseProtocol {
-    
+
     let promise: Promise<JSValue>
-    
+
     init(promise: Promise<JSValue>) {
         self.promise = promise
     }
-    
+
     func then(_ onFulfilled: JSValue, _ onRejected: JSValue) -> JSPromise {
-        
+
         // Keep a reference to the returned promise so we can comply to 2.3.1
         var returnedPromiseRef: Promise<JSValue>?
-        
+
         let afterFulfill = promise.then { value -> Promise<JSValue> in
-            
+
             // 2.2.1: ignored if not a function
             guard JSUtils.isFunction(value: onFulfilled) else {
                 return .value(value)
             }
-            
+
             // Call `onFulfilled`
             // 2.2.5: onFulfilled/onRejected must be called as functions (with no `this` value)
             guard let returnValue = try JSUtils.call(function: onFulfilled, arguments: [JSUtils.undefined, value]) else {
                 return .value(value)
             }
-            
+
             // Extract JSPromise.promise if available, or use plain return value
             if let jsPromise = returnValue.toObjectOf(JSPromise.self) as? JSPromise {
-                
+
                 // 2.3.1: if returned value is the promise that `then` returned, throw TypeError
                 if jsPromise.promise === returnedPromiseRef {
                     throw JSUtils.JSError(reason: JSUtils.typeError(message: "Returned self"))
@@ -53,23 +52,23 @@ class JSPromise: NSObject, JSPromiseProtocol {
                 return .value(returnValue)
             }
         }
-        
+
         let afterReject = promise.recover { error -> Promise<JSValue> in
-            
+
             // 2.2.1: ignored if not a function
             guard let jsError = error as? JSUtils.JSError, JSUtils.isFunction(value: onRejected) else {
                 throw error
             }
-            
+
             // Call `onRejected`
             // 2.2.5: onFulfilled/onRejected must be called as functions (with no `this` value)
             guard let returnValue = try JSUtils.call(function: onRejected, arguments: [JSUtils.undefined, jsError.reason]) else {
                 throw error
             }
-            
+
             // Extract JSPromise.promise if available, or use plain return value
             if let jsPromise = returnValue.toObjectOf(JSPromise.self) as? JSPromise {
-                
+
                 // 2.3.1: if returned value is the promise that `then` returned, throw TypeError
                 if jsPromise.promise === returnedPromiseRef {
                     throw JSUtils.JSError(reason: JSUtils.typeError(message: "Returned self"))
@@ -79,7 +78,7 @@ class JSPromise: NSObject, JSPromiseProtocol {
                 return .value(returnValue)
             }
         }
-        
+
         let newPromise = Promise<Result<JSValue, Error>> { resolver in
             _ = promise.tap(resolver.fulfill)
         }.then(on: nil) { result -> Promise<JSValue> in
@@ -89,7 +88,7 @@ class JSPromise: NSObject, JSPromiseProtocol {
             }
         }
         returnedPromiseRef = newPromise
-        
+
         return JSPromise(promise: newPromise)
     }
 }
