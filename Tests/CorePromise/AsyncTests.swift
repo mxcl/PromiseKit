fdc0cf9ed24d2d0a4730ace433c32e9ca823a703
Roman Podymov <podymfrombryansk@yandex.ru>
2022-07-24 22:02:07 +0200

    Fixing tests with #if

--- Tests/CorePromise/AsyncTests.swift
+++ Tests/CorePromise/AsyncTests.swift
@@ -6,27 +6,35 @@ private enum Error: Swift.Error { case dummy }
 
 class AsyncTests: XCTestCase {
     
+    #if swift(>=5.5)
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func testAsyncPromiseValue() async throws {
-        #if swift(>=5.5)
         let promise = after(.milliseconds(100)).then(on: nil){ Promise.value(1) }
         let value = try await promise.async()
         XCTAssertEqual(value, 1)
-        #endif
     }
+    #else
+    func testAsyncPromiseValue() {
+
+    }
+    #endif
     
+    #if swift(>=5.5)
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func testAsyncGuaranteeValue() async {
-        #if swift(>=5.5)
         let guarantee = after(.milliseconds(100)).then(on: nil){ Guarantee.value(1) }
         let value = await guarantee.async()
         XCTAssertEqual(value, 1)
-        #endif
     }
+    #else
+    func testAsyncGuaranteeValue() {
+
+    }
+    #endif
     
+    #if swift(>=5.5)
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func testAsyncPromiseThrow() async throws {
-        #if swift(>=5.5)
         do {
             let promise = after(.milliseconds(100)).then(on: nil){ Promise(error: Error.dummy) }.then(on: nil){ Promise.value(1) }
             try await _ = promise.async()
@@ -39,7 +47,11 @@ class AsyncTests: XCTestCase {
                 XCTAssert(false)
             }
         }
-        #endif
     }
+    #else
+    func testAsyncPromiseThrow() {
+
+    }
+    #endif
 }
 
