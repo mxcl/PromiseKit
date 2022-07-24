5d7a32aa45537f04a85fe7b8d01173078789c89e
Roman Podymov <podymfrombryansk@yandex.ru>
2022-07-24 15:51:18 +0200

    Fixing tests

--- Tests/CorePromise/AsyncTests.swift
+++ Tests/CorePromise/AsyncTests.swift
@@ -2,27 +2,31 @@ import PromiseKit
 import Dispatch
 import XCTest
 
-#if swift(>=5.5)
 private enum Error: Swift.Error { case dummy }
 
 class AsyncTests: XCTestCase {
     
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func testAsyncPromiseValue() async throws {
+        #if swift(>=5.5)
         let promise = after(.milliseconds(100)).then(on: nil){ Promise.value(1) }
         let value = try await promise.async()
         XCTAssertEqual(value, 1)
+        #endif
     }
     
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func testAsyncGuaranteeValue() async {
+        #if swift(>=5.5)
         let guarantee = after(.milliseconds(100)).then(on: nil){ Guarantee.value(1) }
         let value = await guarantee.async()
         XCTAssertEqual(value, 1)
+        #endif
     }
     
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func testAsyncPromiseThrow() async throws {
+        #if swift(>=5.5)
         do {
             let promise = after(.milliseconds(100)).then(on: nil){ Promise(error: Error.dummy) }.then(on: nil){ Promise.value(1) }
             try await _ = promise.async()
@@ -35,7 +39,7 @@ class AsyncTests: XCTestCase {
                 XCTAssert(false)
             }
         }
+        #endif
     }
 }
 
-#endif
