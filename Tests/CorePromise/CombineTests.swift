bc121036ac0573ef02167d7d4d2a8bf0ffcc4848
Roman Podymov <podymfrombryansk@yandex.ru>
2023-02-06 16:24:48 +0100

    Fix for iOS < 13.0

--- Tests/CorePromise/CombineTests.swift
+++ Tests/CorePromise/CombineTests.swift
@@ -6,66 +6,81 @@ import XCTest
 private enum Error: Swift.Error { case dummy }
 
 class CombineTests: XCTestCase {
-    private var cancellables: Set<AnyCancellable> = []
+    private var cancellable: Any?
     
-    override func setUp() {
-        cancellables = []
+    override func tearDown() {
+        if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
+            (cancellable as? AnyCancellable)?.cancel()
+        }
     }
     
     func testCombinePromiseValue() {
         let ex = expectation(description: "")
-        let promise = after(.milliseconds(100)).then(on: nil){ Promise.value(1) }
-        promise.future().sink { result in
-            switch result {
-            case .failure:
-                XCTAssert(false)
-            default:
-                XCTAssert(true)
+        if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
+            let promise = after(.milliseconds(100)).then(on: nil){ Promise.value(1) }
+            cancellable = promise.future().sink { result in
+                switch result {
+                case .failure:
+                    XCTAssert(false)
+                default:
+                    XCTAssert(true)
+                }
+            } receiveValue: {
+                XCTAssertEqual($0, 1)
+                ex.fulfill()
             }
-        } receiveValue: {
-            XCTAssertEqual($0, 1)
+        } else {
             ex.fulfill()
-        }.store(in: &cancellables)
+        }
 
         wait(for: [ex], timeout: 1)
     }
     
     func testCombineGuaranteeValue() {
         let ex = expectation(description: "")
-        let promise = after(.milliseconds(100)).then(on: nil){ Guarantee.value(1) }
-        promise.future().sink { result in
-            switch result {
-            case .failure:
-                XCTAssert(false)
-            default:
-                XCTAssert(true)
+        if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
+            let promise = after(.milliseconds(100)).then(on: nil){ Guarantee.value(1) }
+            cancellable = promise.future().sink { result in
+                switch result {
+                case .failure:
+                    XCTAssert(false)
+                default:
+                    XCTAssert(true)
+                }
+            } receiveValue: {
+                XCTAssertEqual($0, 1)
+                ex.fulfill()
             }
-        } receiveValue: {
-            XCTAssertEqual($0, 1)
+        } else {
             ex.fulfill()
-        }.store(in: &cancellables)
+        }
 
         wait(for: [ex], timeout: 1)
     }
     
     func testCombinePromiseThrow() {
         let ex = expectation(description: "")
-        let promise = after(.milliseconds(100)).then(on: nil){ Promise(error: Error.dummy) }.then(on: nil){ Promise.value(1) }
-        promise.future().sink { result in
-            switch result {
-            case .failure(let error):
-                switch error as? Error {
-                case .dummy:
-                    XCTAssert(true)
+        if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
+            let promise = after(.milliseconds(100)).then(on: nil){ Promise(error: Error.dummy) }.then(on: nil){ Promise.value(1) }
+            cancellable = promise.future().sink { result in
+                switch result {
+                case .failure(let error):
+                    switch error as? Error {
+                    case .dummy:
+                        XCTAssert(true)
+                    default:
+                        XCTAssert(false)
+                    }
                 default:
                     XCTAssert(false)
                 }
-            default:
+                ex.fulfill()
+            } receiveValue: { _ in
                 XCTAssert(false)
             }
-        } receiveValue: { _ in
-            XCTAssert(false)
-        }.store(in: &cancellables)
+        } else {
+            ex.fulfill()
+        }
 
         wait(for: [ex], timeout: 1)
     }
