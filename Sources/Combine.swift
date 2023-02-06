07f21b2a78dbe1679b86efff4911d1b67f668aa9
Roman Podymov <podymfrombryansk@yandex.ru>
2023-02-06 17:29:32 +0100

    Fix for Linux

--- Sources/Combine.swift
+++ Sources/Combine.swift
@@ -1,12 +1,12 @@
-#if swift(>=4.1)
+#if !os(Linux) && swift(>=4.1)
 #if canImport(Combine)
 import Combine
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension Guarantee {
     func future() -> Future<T, Never> {
-        .init { promise in
-            self.done { value in
+        .init { [weak self] promise in
+            self?.done { value in
                 promise(.success(value))
             }
         }
@@ -16,8 +16,8 @@ public extension Guarantee {
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension Promise {
     func future() -> Future<T, Error> {
-        .init { promise in
-            self.done { value in
+        .init { [weak self] promise in
+            self?.done { value in
                 promise(.success(value))
             }.catch { error in
                 promise(.failure(error))
--- Tests/CorePromise/CombineTests.swift
+++ Tests/CorePromise/CombineTests.swift
@@ -1,4 +1,4 @@
-#if swift(>=4.1)
+#if !os(Linux) && swift(>=4.1)
 #if canImport(Combine)
 import Combine
 #endif
@@ -19,7 +19,7 @@ class CombineTests: XCTestCase {
     
     func testCombinePromiseValue() {
         let ex = expectation(description: "")
-        #if swift(>=4.1)
+        #if !os(Linux) && swift(>=4.1)
         #if canImport(Combine)
         if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
             let promise = after(.milliseconds(100)).then(on: nil){ Promise.value(1) }
@@ -49,7 +49,7 @@ class CombineTests: XCTestCase {
     
     func testCombineGuaranteeValue() {
         let ex = expectation(description: "")
-        #if swift(>=4.1)
+        #if !os(Linux) && swift(>=4.1)
         #if canImport(Combine)
         if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
             let promise = after(.milliseconds(100)).then(on: nil){ Guarantee.value(1) }
@@ -79,7 +79,7 @@ class CombineTests: XCTestCase {
     
     func testCombinePromiseThrow() {
         let ex = expectation(description: "")
-        #if swift(>=4.1)
+        #if !os(Linux) && swift(>=4.1)
         #if canImport(Combine)
         if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
             let promise = after(.milliseconds(100)).then(on: nil){ Promise(error: Error.dummy) }.then(on: nil){ Promise.value(1) }
