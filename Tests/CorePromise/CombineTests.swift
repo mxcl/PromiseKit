8992343a9a06cf1504390bc6d99251a25cdbd2e5
Roman Podymov <podymfrombryansk@yandex.ru>
2023-02-06 18:04:46 +0100

    receiveCompletion

--- Tests/CorePromise/CombineTests.swift
+++ Tests/CorePromise/CombineTests.swift
@@ -32,17 +32,17 @@ class CombineTests: XCTestCase {
         #if canImport(Combine)
         if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
             let promise = after(.milliseconds(100)).then(on: nil){ Promise.value(1) }
-            cancellable = promise.future().sink { result in
+            cancellable = promise.future().sink(receiveCompletion: { result in
                 switch result {
                 case .failure:
                     XCTAssert(false)
                 default:
                     XCTAssert(true)
                 }
-            } receiveValue: {
+            }, receiveValue: {
                 XCTAssertEqual($0, 1)
                 ex.fulfill()
-            }
+            })
         } else {
             ex.fulfill()
         }
@@ -66,17 +66,17 @@ class CombineTests: XCTestCase {
         #if canImport(Combine)
         if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
             let promise = after(.milliseconds(100)).then(on: nil){ Guarantee.value(1) }
-            cancellable = promise.future().sink { result in
+            cancellable = promise.future().sink(receiveCompletion: { result in
                 switch result {
                 case .failure:
                     XCTAssert(false)
                 default:
                     XCTAssert(true)
                 }
-            } receiveValue: {
+            }, receiveValue: {
                 XCTAssertEqual($0, 1)
                 ex.fulfill()
-            }
+            })
         } else {
             ex.fulfill()
         }
@@ -100,7 +100,7 @@ class CombineTests: XCTestCase {
         #if canImport(Combine)
         if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
             let promise = after(.milliseconds(100)).then(on: nil){ Promise(error: Error.dummy) }.then(on: nil){ Promise.value(1) }
-            cancellable = promise.future().sink { result in
+            cancellable = promise.future().sink(receiveCompletion: { result in
                 switch result {
                 case .failure(let error):
                     switch error as? Error {
@@ -113,9 +113,9 @@ class CombineTests: XCTestCase {
                     XCTAssert(false)
                 }
                 ex.fulfill()
-            } receiveValue: { _ in
+            }, receiveValue: { _ in
                 XCTAssert(false)
-            }
+            })
         } else {
             ex.fulfill()
         }
