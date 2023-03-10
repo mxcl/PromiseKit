a067d9777ebc6b717a2518e0c8f87eb363fea8d5
Roman Podymov <podymfrombryansk@yandex.ru>
2023-03-10 11:02:54 +0100

    Future extensions

--- Sources/Combine.swift
+++ Sources/Combine.swift
@@ -25,5 +25,42 @@ public extension Promise {
         }
     }
 }
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public extension Future {
+    func promise() -> PromiseKit.Promise<Output> {
+        return .init { [weak self] resolver in
+            var cancellable: AnyCancellable?
+            cancellable = self?.sink(receiveCompletion: { completion in
+                cancellable?.cancel()
+                cancellable = nil
+                switch completion {
+                case .failure(let error):
+                    resolver.reject(error)
+                case .finished:
+                    break
+                }
+            }, receiveValue: { value in
+                cancellable?.cancel()
+                cancellable = nil
+                resolver.fulfill(value)
+            })
+        }
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public extension Future where Failure == Never {
+    func guarantee() -> Guarantee<Output> {
+        return .init { [weak self] resolver in
+            var cancellable: AnyCancellable?
+            cancellable = self?.sink(receiveValue: { value in
+                cancellable?.cancel()
+                cancellable = nil
+                resolver(value)
+            })
+        }
+    }
+}
 #endif
 #endif
